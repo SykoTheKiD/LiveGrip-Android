e49ce9712357b7df06fc771571d20cfd33c6999a
Jay SyKo <jay@jaysyko.com>
2016-01-25 21:23:10 -0500

    Basic Chat Added

--- app/src/main/java/com/jaysyko/wrestlechat/MainActivity.java
+++ app/src/main/java/com/jaysyko/wrestlechat/MainActivity.java
@@ -3,12 +3,14 @@ package com.jaysyko.wrestlechat;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class MainActivity extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(Message.class);
         Parse.initialize(this);
     }
 }
