682b85465178c5a1e4d87d319bc545a417e4cb62
Jay SyKo <jay@jaysyko.com>
2016-01-27 17:21:30 -0500

    Added New Event Info View

--- app/src/main/java/com/jaysyko/wrestlechat/activities/EventListActivity.java
+++ app/src/main/java/com/jaysyko/wrestlechat/activities/EventListActivity.java
@@ -1,4 +1,4 @@
-package com.jaysyko.wrestlechat;
+package com.jaysyko.wrestlechat.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.jaysyko.wrestlechat.R;
 import com.parse.FindCallback;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
