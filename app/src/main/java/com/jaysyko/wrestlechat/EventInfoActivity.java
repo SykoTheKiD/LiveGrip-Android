7fd46ea848cbb367a2eca5b89b36c8d4e4d9e35d
Jay SyKo <jay@jaysyko.com>
2016-01-27 11:29:26 -0500

    Added Sticky Event Info Page

--- app/src/main/java/com/jaysyko/wrestlechat/EventInfoActivity.java
+++ app/src/main/java/com/jaysyko/wrestlechat/EventInfoActivity.java
@@ -1,77 +1,81 @@
 package com.jaysyko.wrestlechat;
 
-import android.graphics.Rect;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.view.ViewGroup;
+import android.view.View;
 import android.widget.AbsListView;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class EventInfoActivity extends AppCompatActivity implements AbsListView.OnScrollListener {
+public class EventInfoActivity extends AppCompatActivity {
 
-    private static final int MAX_ROWS = 50;
-    private int lastTopValue = 0;
+    private TextView stickyView;
+    private ListView listView;
+    private View heroImageView;
 
-    private List<String> modelList = new ArrayList<>();
-    private ImageView backgroundImage;
+    private View stickyViewSpacer;
+
+    private int MAX_ROWS = 20;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_info);
-        ListView listView = (ListView) findViewById(R.id.list);
-        setTitle("WWE Royal Rumble 2016");
-        modelList.add("Royal Rumble (2016) was a professional wrestling pay-per-view (PPV) event produced by WWE. It took place on January 24, 2016, at Amway Center in Orlando, Florida. It was the 29th event in the Royal Rumble chronology. This was the fifth Royal Rumble to be held in the state of Florida (1990, 1991, 1995, and 2006) and the first pay-per-view event at Amway Center since it opened in 2010.\n" +
-                "\n" +
-                "At the event, Roman Reigns defended his WWE World Heavyweight Championship in the Royal Rumble match. The event also marked the first time that a reigning champion has defended his title during the match and the second time (after the 1992 event) that the title is the prize for winning the match.\n" +
-                "\n" +
-                "Six matches were contested at the event, with one match contested on the pre-show. In the main event, the returning Triple H won the Royal Rumble match by eliminating Dean Ambrose to win the WWE World Heavyweight Championship, becoming the third person to win from entrant number 30 and the first since John Cena in 2008. The defending champion Roman Reigns was also eliminated by Triple H.\n" +
-                "\n" +
-                "The Royal Rumble featured the WWE debut of longtime Total Nonstop Action Wrestling (TNA) mainstay A.J. Styles.");
+
+        /* Initialise list view, hero image, and sticky view */
+        listView = (ListView) findViewById(R.id.listView);
+        heroImageView = findViewById(R.id.heroImageView);
+        stickyView = (TextView) findViewById(R.id.stickyView);
+
+        /* Inflate list header layout */
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View listHeader = inflater.inflate(R.layout.event_info_header, null);
+        stickyViewSpacer = listHeader.findViewById(R.id.stickyViewPlaceholder);
+
+        /* Add list view header */
+        listView.addHeaderView(listHeader);
+
+        /* Handle list View scroll events */
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+
+                /* Check if the first item is already reached to top.*/
+                if (listView.getFirstVisiblePosition() == 0) {
+                    View firstChild = listView.getChildAt(0);
+                    int topY = 0;
+                    if (firstChild != null) {
+                        topY = firstChild.getTop();
+                    }
+
+                    int heroTopY = stickyViewSpacer.getTop();
+                    stickyView.setY(Math.max(0, heroTopY + topY));
+
+                    /* Set the image to scroll half of the amount that of ListView */
+                    heroImageView.setY(topY * 0.5f);
+                }
+            }
+        });
+
+
+        /* Populate the ListView with sample data */
+        List<String> modelList = new ArrayList<>();
         for (int i = 0; i < MAX_ROWS; i++) {
             modelList.add("List item " + i);
         }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.event_info_list_item, modelList);
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.event_info_list_item, modelList);
         listView.setAdapter(adapter);
-
-        // inflate custom header and attach it to the list
-        LayoutInflater inflater = getLayoutInflater();
-        ViewGroup header = (ViewGroup) inflater.inflate(R.layout.event_info_header, listView, false);
-        listView.addHeaderView(header, null, false);
-
-        // we take the background image and button reference from the header
-        backgroundImage = (ImageView) header.findViewById(R.id.listHeaderImage);
-        listView.setOnScrollListener(this);
     }
-
-
-    @Override
-    public void onScrollStateChanged(AbsListView view, int scrollState) {
-
-    }
-
-    @Override
-    public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
-        Rect rect = new Rect();
-        backgroundImage.getLocalVisibleRect(rect);
-        if (lastTopValue != rect.top) {
-            lastTopValue = rect.top;
-            backgroundImage.setY((float) (rect.top / 2.0));
-        }
-    }
-
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_event_info);
-//        getActionBar().setDisplayHomeAsUpEnabled(true);
-//    }
-
 }
