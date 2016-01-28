794c5512bf7308bae2a9cc4b342b51bad5ddc712
Jay SyKo <jay@jaysyko.com>
2016-01-28 18:29:22 -0500

    Removed Logout Snackbar

--- app/src/main/java/com/jaysyko/wrestlechat/activities/EventListActivity.java
+++ app/src/main/java/com/jaysyko/wrestlechat/activities/EventListActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -53,8 +52,6 @@ public class EventListActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Logged Out", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
                 ParseUser.logOut();
                 Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
