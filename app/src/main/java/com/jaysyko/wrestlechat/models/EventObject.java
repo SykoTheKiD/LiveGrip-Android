db90f4cdaba3c783c3980e552544f5a3dc91d6a5
Jay SyKo <jay@jaysyko.com>
2016-01-29 11:06:48 -0500

    Added Imgur Images

--- app/src/main/java/com/jaysyko/wrestlechat/models/EventObject.java
+++ app/src/main/java/com/jaysyko/wrestlechat/models/EventObject.java
@@ -5,16 +5,22 @@ public class EventObject {
     private String location;
     private String startTime;
     private String endTime;
+    private String imageLink;
 
-    public EventObject(String eventName, String location, String startTime, String endTime) {
+    public EventObject(String eventName, String location, String startTime, String endTime, String imageLink) {
         this.eventName = eventName;
         this.location = location;
         this.startTime = startTime;
         this.endTime = endTime;
+        this.imageLink = imageLink;
     }
 
     public String getEventName() {
         return this.eventName;
+    }
+
+    public String getImageLink() {
+        return this.imageLink;
     }
 
     public String getLocation() {
