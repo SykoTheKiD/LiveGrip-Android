d55d772d99a0233d1137a393548b18f48c259682
Jay SyKo <jay@jaysyko.com>
2016-01-31 21:59:42 -0500

    Added Event long press vibrate

--- app/src/main/java/com/jaysyko/wrestlechat/activities/EventInfoActivity.java
+++ app/src/main/java/com/jaysyko/wrestlechat/activities/EventInfoActivity.java
@@ -18,7 +18,7 @@ public class EventInfoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_event_info);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setTitle(getIntent().getStringExtra(IntentKeys.EVENT_NAME_INTENT_KEY));
+        setTitle(getIntent().getStringExtra(IntentKeys.EVENT_NAME));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
