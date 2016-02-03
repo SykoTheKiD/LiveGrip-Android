0d596cf64afeb4b6d4d6e706c4b52d5da46ff11f
Jay SyKo <jay@jaysyko.com>
2016-02-03 00:33:52 -0500

    Added Go Live Function

--- app/src/main/java/com/jaysyko/wrestlechat/adapters/EventListAdapter.java
+++ app/src/main/java/com/jaysyko/wrestlechat/adapters/EventListAdapter.java
@@ -1,23 +1,28 @@
 package com.jaysyko.wrestlechat.adapters;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
 import com.jaysyko.wrestlechat.R;
 import com.jaysyko.wrestlechat.dataObjects.Event;
+import com.jaysyko.wrestlechat.utils.DateChecker;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
 
 public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder> {
     private static final String IMGUR_LINK = "http://i.imgur.com/";
+    private static final String NON_LIVE_TEXT_COLOUR = "#bdbdbd";
+    private static final int NON_LIVE_TEXT_SIZE = 15;
     private List<Event> itemsData;
     private Context context;
+
 
     public EventListAdapter(List<Event> itemsData, Context context) {
         this.itemsData = itemsData;
@@ -40,13 +45,21 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
     // Replace the contents of a view (invoked by the layout manager)
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int position) {
-
         // - get data from your itemsData at this position
         // - replace the contents of the view with that itemsData
         Event currentCard = itemsData.get(position);
         viewHolder.txtViewTitle.setText(currentCard.getEventName());
         viewHolder.txtViewLocation.setText(currentCard.getLocation());
         Picasso.with(this.context).load(IMGUR_LINK.concat(currentCard.getImageLink())).into(viewHolder.imgViewIcon);
+
+        if (DateChecker.goLive(currentCard.getStartTime())) {
+            viewHolder.txtViewLiveStatus.setText(R.string.online_status_live);
+        } else {
+            String eventDate = DateChecker.format(currentCard.getStartTime());
+            viewHolder.txtViewLiveStatus.setTextColor(Color.parseColor(NON_LIVE_TEXT_COLOUR));
+            viewHolder.txtViewLiveStatus.setTextSize(TypedValue.COMPLEX_UNIT_SP, NON_LIVE_TEXT_SIZE);
+            viewHolder.txtViewLiveStatus.setText(eventDate);
+        }
     }
 
     // Return the size of your itemsData (invoked by the layout manager)
@@ -61,12 +74,14 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
         public TextView txtViewTitle;
         public ImageView imgViewIcon;
         public TextView txtViewLocation;
+        public TextView txtViewLiveStatus;
 
         public ViewHolder(View itemLayoutView) {
             super(itemLayoutView);
             txtViewTitle = (TextView) itemLayoutView.findViewById(R.id.event_name);
             imgViewIcon = (ImageView) itemLayoutView.findViewById(R.id.event_image);
             txtViewLocation = (TextView) itemLayoutView.findViewById(R.id.event_location);
+            txtViewLiveStatus = (TextView) itemLayoutView.findViewById(R.id.event_live);
         }
     }
 }
