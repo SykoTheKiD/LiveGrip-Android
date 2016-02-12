73caaf143727a01849b9b1924b4e77f17f3dc6f5
Jay SyKo <jay@jaysyko.com>
2016-02-11 21:53:35 -0500

    Added more form validation

--- app/src/main/java/com/jaysyko/wrestlechat/utils/FormValidation.java
+++ app/src/main/java/com/jaysyko/wrestlechat/utils/FormValidation.java
@@ -2,6 +2,10 @@ package com.jaysyko.wrestlechat.utils;
 
 public class FormValidation {
 
+    private static final int MIN_MESSAGE_LENGTH = 1;
+    private static final int MAX_MESSAGE_LENGTH = 22;
+    private static final int MAX_USERNAME_LENGTH = 10;
+
     public static boolean formIsClean(String... strings) {
         for (String str : strings) {
             if (str == null || str.isEmpty()) {
@@ -9,5 +13,13 @@ public class FormValidation {
             }
         }
         return true;
+    }
+
+    public static boolean isValidMessage(String message) {
+        return message.length() > MIN_MESSAGE_LENGTH && message.length() < MAX_MESSAGE_LENGTH;
+    }
+
+    public static boolean isValidUsername(String username) {
+        return username.length() < MAX_USERNAME_LENGTH;
     }
 }
