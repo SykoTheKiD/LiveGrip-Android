e0d01c29ffcac4f5c4155d29d548e462f680d51f
Jay SyKo <jay@jaysyko.com>
2016-02-16 10:50:46 -0500

    Added new message layout

--- app/src/main/java/com/jaysyko/wrestlechat/activities/LoginActivity.java
+++ app/src/main/java/com/jaysyko/wrestlechat/activities/LoginActivity.java
@@ -50,10 +50,8 @@ public class LoginActivity extends AppCompatActivity {
                     username = usernameField.getText().toString();
                     password = passwordField.getText().toString();
                     showLoadPanel(true, loginButton);
-                    Log.d(username,password);
                     if (NetworkState.isConnected(context)) {
                         Form form = FormValidation.validateLogin(username, password);
-                        Log.d(username,password);
                         if (form.valid) {
                             CurrentActiveUser currentActiveUser = CurrentActiveUser.getInstance(username, password);
                             if (currentActiveUser.loginUser()) {
