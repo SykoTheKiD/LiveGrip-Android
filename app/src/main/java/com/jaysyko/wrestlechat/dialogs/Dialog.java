97019ee0ee066ec3801c9b892f6f868f783fec0b
Jay SyKo <jay@jaysyko.com>
2016-02-16 12:12:42 -0500

    Added Alert Dialog

--- app/src/main/java/com/jaysyko/wrestlechat/dialogs/Dialog.java
+++ app/src/main/java/com/jaysyko/wrestlechat/dialogs/Dialog.java
@@ -1,17 +1,34 @@
 package com.jaysyko.wrestlechat.dialogs;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.design.widget.Snackbar;
 import android.view.View;
 import android.widget.Toast;
+
+import com.jaysyko.wrestlechat.R;
 
 public class Dialog {
     public static void makeToast(Context context, String msg) {
         Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
     }
 
-    public static void makeSnackBar(View view) {
-        Snackbar snackbar = Snackbar.make(view, "Internet Disconnected", Snackbar.LENGTH_INDEFINITE);
+    public static void makeSnackBar(View view, String message) {
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_INDEFINITE);
         snackbar.show();
+    }
+
+    public static void makeDialog(Context context, String title, String bodyContent) {
+        new AlertDialog.Builder(context)
+                .setTitle(title)
+                .setMessage(bodyContent)
+                .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with delete
+                    }
+                })
+                .setIcon(R.drawable.ic_face_black_18dp)
+                .show();
     }
 }
