840dd8ff8ac97b97fd48c16520b09f9c50bdde3a
Jay SyKo <jay@jaysyko.com>
2016-02-28 21:47:07 -0500

    Removed unneeded code

--- app/src/main/java/com/jaysyko/wrestlechat/activities/EventInfoActivity.java
+++ app/src/main/java/com/jaysyko/wrestlechat/activities/EventInfoActivity.java
@@ -58,12 +58,6 @@ public class EventInfoActivity extends AppCompatActivity {
                 new AdBuilder(EventInfoActivity.this).buildAd();
             }
         });
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                new AdBuilder(EventInfoActivity.this).buildAd();
-            }
-        });
     }
 
     @NonNull
