412a2f9b7b8a87b7680726eae7b72d7174bf56ee
Jay SyKo <jay@jaysyko.com>
2016-03-09 16:00:52 -0500

    Fixed Layout

--- app/src/main/java/com/jaysyko/wrestlechat/activities/MessagingActivity.java
+++ app/src/main/java/com/jaysyko/wrestlechat/activities/MessagingActivity.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.jaysyko.wrestlechat.R;
+import com.jaysyko.wrestlechat.activeEvent.CurrentActiveEvent;
 import com.jaysyko.wrestlechat.fragments.MessagingFragment;
 
 public class MessagingActivity extends AppCompatActivity {
@@ -18,12 +20,14 @@ public class MessagingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messaging);
-
+        String eventName = CurrentActiveEvent.getInstance().getEventName();
+        setTitle(eventName);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
         FragmentTransaction mFragmentTransaction = getSupportFragmentManager().beginTransaction();
         mFragmentTransaction.replace(R.id.container_view, new MessagingFragment()).commit();
 
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -43,23 +47,4 @@ public class MessagingActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
     }
-//
-//    public void onDestroy() {
-//    super.onDestroy();
-//    closeAllThreads();
-//}
-//
-//    public void onPause() {
-//        super.onPause();
-//        closeAllThreads();
-//    }
-//
-//    public void onResume() {
-//        super.onResume();
-//        handler.post(fetchNewMessagesRunnable);
-//    }
-
-//    private void closeAllThreads() {
-//        handler.removeCallbacks(fetchNewMessagesRunnable);
-//    }
 }
