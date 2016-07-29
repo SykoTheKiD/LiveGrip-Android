6c7e577591de6c6200979cf19858be7a654ca6c3
Jay SyKo <jay@jaysyko.com>
2016-07-29 00:13:24 -0400

    Updated Version Number

--- app/src/main/java/com/jaysyko/wrestlechat/application/App.java
+++ app/src/main/java/com/jaysyko/wrestlechat/application/App.java
@@ -5,7 +5,7 @@ package com.jaysyko.wrestlechat.application;
  */
 public class App {
 
-    public static final String APP_VERSION = "1";
+    public static final String APP_VERSION = "1.0.1";
 
     public static boolean debug = true;
 }
