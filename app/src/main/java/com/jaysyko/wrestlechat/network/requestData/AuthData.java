b94b939f918416e32e7d60be5f63df3c2e748ed1
Jay SyKo <jay@jaysyko.com>
2016-07-31 12:09:54 -0400

    Added App Versioning

--- app/src/main/java/com/jaysyko/wrestlechat/network/requestData/AuthData.java
+++ app/src/main/java/com/jaysyko/wrestlechat/network/requestData/AuthData.java
@@ -1,5 +1,7 @@
 package com.jaysyko.wrestlechat.network.requestData;
 
+import com.jaysyko.wrestlechat.application.App;
+
 /**
  * Created by jarushaan on 2016-06-03
  */
@@ -7,10 +9,20 @@ public class AuthData {
 
     private String username;
     private String password;
+    private String app_version;
 
     public AuthData(String username, String password){
         this.username = username;
         this.password = password;
+        this.app_version = App.APP_VERSION;
+    }
+
+    public String getApp_version() {
+        return app_version;
+    }
+
+    public void setApp_version(String app_version) {
+        this.app_version = app_version;
     }
 
     public String getUsername() {
